07939197ab7abc0dd1624fe64f8c6917881f1240
luke-the-coder <94640753+luke-the-coder@users.noreply.github.com>
2023-02-13 12:18:06 +0100

    First commit, this is just the xcode template

---
+++ MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by Fabio Luca Uccella on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
