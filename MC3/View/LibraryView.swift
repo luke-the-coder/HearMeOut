3da41468423252444f779eba6945bd82e4ee6990
luke-the-coder <94640753+luke-the-coder@users.noreply.github.com>
2023-03-05 19:49:58 +0100

    Added deeplink

--- MC3/View/LibraryView.swift
+++ MC3/View/LibraryView.swift
@@ -12,65 +12,83 @@ import XMLCoder
 
 struct LibraryView: View {
     @State private var openFile = false
+    @State var url: URL?
     @StateObject private var viewModel = ScoreStore()
     @State private var selectedFile: IdentifiedURL?
     var body: some View {
         NavigationStack {
-            List(){
-                ForEach(viewModel.filteredScores) { score in
-                    if let destination = ScoreView(url: score.path!) {
-                        NavigationLink(destination: destination) {
-                            ScoreRowView(score: score)
-                        }
-                    }
-                }
-                .onDelete(perform: viewModel.deleteItems)
-            }
-            .navigationTitle("Music Reader").toolbar{
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add") {
-                        self.openFile.toggle()
-                    }
-                }
-            }
-            .searchable(text: $viewModel.searchText, prompt: "Songs, Composer...")
-            .fileImporter(isPresented: $openFile, allowedContentTypes: [UTType(filenameExtension: "musicxml")!]) { (res) in
-                    
-                    do{
-                        let fileUrl = try res.get()
-                        print(fileUrl)
-                        
-                        guard fileUrl.startAccessingSecurityScopedResource() else { return }
-                        self.selectedFile = IdentifiedURL(url: fileUrl)
-                        do {
-                            let musicScore = try MusicXMLDecoder.decode(type: ScoreModel.self, from: fileUrl)
-                            if (musicScore.movementTitle != nil) {
-                                viewModel.checkSavedScores(fileURL: fileUrl, Title: (musicScore.movementTitle) ?? "null", Author:  (musicScore.identification?.creator) ?? "null")
-                            } else {
-                                viewModel.checkSavedScores(fileURL: fileUrl, Title: (musicScore.work?.workTitle) ?? "null", Author:  (musicScore.identification?.creator) ?? "null")
+            if let url = url {
+                ScoreView(url: url)
+            } else {
+                List(){
+                    ForEach(viewModel.filteredScores) { score in
+                        if let destination = ScoreView(url: score.path!) {
+                            NavigationLink(destination: destination) {
+                                ScoreRowView(score: score)
                             }
-                        } catch {
-                            print(error.localizedDescription)
                         }
-                        fileUrl.stopAccessingSecurityScopedResource()
-                    } catch{
-                        print ("error reading")
-                        print (error.localizedDescription)
+                    }
+                    .onDelete(perform: viewModel.deleteItems)
+                }.navigationTitle("Music Reader").toolbar{
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Add") {
+                            self.openFile.toggle()
+                        }
                     }
                 }
+            }
         }
         
+        .searchable(text: $viewModel.searchText, prompt: "Songs, Composer...")
+        .fileImporter(isPresented: $openFile, allowedContentTypes: [UTType(filenameExtension: "musicxml")!]) { (res) in
+            do {
+                let fileUrl = try res.get()
+                MusicScoreLoader().loadMusicScore(url: fileUrl, viewModel: viewModel)
+            } catch {
+                print ("error reading")
+                print (error.localizedDescription)
+            }
+        }
+        .onOpenURL{ url in
+            MusicScoreLoader().loadMusicScore(url: url, viewModel: viewModel)
+            self.url = url
+        }
     }
 }
+
+
+struct MusicScoreLoader {
+    
+    func loadMusicScore(url: URL, viewModel: ScoreStore) {
+        do {
+            guard url.startAccessingSecurityScopedResource() else { return }
+            do {
+                let musicScore = try MusicXMLDecoder.decode(type: ScoreModel.self, from: url)
+                if (musicScore.movementTitle != nil) {
+                    viewModel.checkSavedScores(fileURL: url, Title: (musicScore.movementTitle) ?? "null", Author:  (musicScore.identification?.creator) ?? "null")
+                } else {
+                    viewModel.checkSavedScores(fileURL: url, Title: (musicScore.work?.workTitle) ?? "null", Author:  (musicScore.identification?.creator) ?? "null")
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+            url.stopAccessingSecurityScopedResource()
+        }
+    }
+}
+
 //struct LibraryView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        LibraryView()
 //    }
 //}
+
 struct IdentifiedURL: Identifiable {
     var id = UUID()
     var url: URL
 }
+
+
 
 private struct ScoreRowView: View {
     let score: Score
@@ -89,8 +107,7 @@ private struct ScoreRowView: View {
 
                 }
             }
-            
             Spacer()
-        
+
     }
 }
